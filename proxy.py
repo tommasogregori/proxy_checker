aad30a4e344f729589a11a61722aef6cb5a3888e
tommasogregori <47010993+tommasogregori@users.noreply.github.com>
2020-04-22 15:59:59 +0200

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,170 +1,24 @@
-# Network
-import urllib.request, urllib.parse, urllib.error
-import http.cookiejar
-import time, itertools, threading, queue
-from colorama import Fore, Back, Style
+import requests, threading
 
-in_directory = input(str("In which directory is the proxy located? (Just the folder's directory), call the proxy file 'proxy.txt' "
-out_filename = 'tested.txt'
-test_url = 'http://www.google.com/humans.txt'
-thread_number = 10
-timeout_value = 6
+url = "https://www.instafollowerspro.com:443/api/sharing-api/v5.0.1/order-action.php"
 
-ok_msg = Fore.GREEN + "OK!  " + Fore.RESET
-fail_msg = Fore.RED + "FAIL " + Fore.RESET
+payload_like = "{\"accestkone\":\"aab7496f658668676067f362c132931b\",\"orderData\":{\"order_id\":\"c0G4avFS2dqiJ1pdzqbhjw==\",\"order_user_id\":\"+j+hnQx9Wl83MWeM92tyZA==\"},\"orderAction\":1}"
+payload_follower = "{\"accestkone\":\"aab7496f658668676067f362c132931b\",\"orderData\":{\"order_id\":\"lJsFziTUF4LtFzww7d2tVw==\",\"order_user_id\":\"+j+hnQx9Wl83MWeM92tyZA==\"},\"orderAction\":1}"
 
-# Stats
-good_proxy_num = itertools.count()
-start_time = time.time()
-end_time   = time.time()
+headers = {
+    "Cookie": "__cfduid=dcf3d8aad5722c504b8dda7a32b26b1841587388558; expires=Wed, 20-May-20 13:15:58 GMT; path=/; domain=.instafollowerspro.com; HttpOnly; SameSite=Lax; user_account_id=60139; expires=Tue, 20-Apr-2021 19:04:45 GMT; Max-Age=31556926; path=/api/manual-api/v3.2/; domain=instafollowerspro.com; secure; HttpOnly; csrftoken=d908422a129560b1c89d3d3e97f512c2; expires=Tue, 20-Apr-2021 19:04:45 GMT; Max-Age=31556926; path=/api/manual-api/v3.2/; domain=instafollowerspro.com; secure; HttpOnly;",
+    "Connection": "close",
+    "User-Agent": "okhttp/3.10.0",
+    "Host": "www.instafollowerspro.com",
+    "Accept-Encoding": "gzip, deflate",
+    "Content-Length": "160",
+    "Content-Type": "application/json; charset=utf-8"
+}
 
-# Safe print()
-mylock = threading.Lock()
-def sprint(*a, **b):
-    with mylock:
-        print(*a, **b)
+def send_post():
+    while 1:
+        requests.request("POST", url, data=payload_follower, headers=headers)
 
-
-#
-# Printer
-#
-class PrintThread(threading.Thread):
-    def __init__(self, queue, filename):
-        threading.Thread.__init__(self)
-        self.queue = queue
-        self.output = open(filename, 'a')
-        self.shutdown = False
-
-    def write(self, line):
-        print(line, file=self.output)
-
-    def run(self):
-        while not self.shutdown:
-            lines = self.queue.get()
-            self.write(lines)
-            self.queue.task_done()
-
-    def terminate(self):
-        self.output.close()
-        self.shutdown = True
-
-
-
-#
-# Processor
-#
-class ProcessThread(threading.Thread):
-    def __init__(self, id, task_queue, out_queue):
-        threading.Thread.__init__(self)
-        self.task_queue = task_queue
-        self.out_queue  = out_queue
-        self.id = id
-
-    # ...
-    def run(self):
-        while True:
-            task   = self.task_queue.get()
-            result = self.process(task)
-
-            if result is not None:
-                self.out_queue.put(result)
-                next(good_proxy_num)
-
-            self.task_queue.task_done()
-
-
-    # Do the processing job here
-    def process(self, task):
-        proxy = task
-        log_msg = str("Thread #%3d.  Trying HTTP proxy %21s \t\t" % (self.id, proxy))
-
-        cj =  http.cookiejar.CookieJar()
-        opener = urllib.request.build_opener(
-                    urllib.request.HTTPCookieProcessor(cj),
-                    urllib.request.HTTPRedirectHandler(),
-                    urllib.request.ProxyHandler({ 'http' : proxy })
-        )
-
-        try:
-            t1 = time.time()
-            response = opener.open(test_url, timeout=timeout_value).read()
-            t2 = time.time()
-        except Exception as e:
-            log_msg += "%s (%s)" % (fail_msg, str(e))
-            sprint(log_msg)
-            return None
-
-        log_msg += ok_msg + " Response time: %d, length=%s" % ( int((t2-t1)*1000), str(len(response)) )
-        sprint(log_msg)
-        return proxy
-
-    def terminate(self):
-        None
-        #print("Thread #%d is down..." % (self.id))
-
-#
-# Main starts here
-#
-# Init some stuff
-input_queue  = queue.Queue()
-result_queue = queue.Queue()
-
-
-# Spawn worker threads
-workers = []
-for i in range(0, thread_number):
-    t = ProcessThread(i, input_queue, result_queue)
-    t.setDaemon(True)
-    t.start()
-    workers.append(t)
-
-# Spawn printer thread to print
-f_printer = PrintThread(result_queue, out_filename)
-f_printer.setDaemon(True)
-f_printer.start()
-
-# Add some stuff to the input queue
-start_time = time.time()
-
-proxy_list = []
-import os
-for root, dirs, files in os.walk(in_directory):
-    for file in files:
-        if file.endswith(".txt"):
-            # read all lines from file
-            file_line_list = [line.rstrip('\n') for line in open(os.path.join(root, file), 'r')]
-            # append to proxy_list
-            proxy_list.extend(file_line_list)
-
-for proxy in proxy_list:
-    input_queue.put(proxy)
-
-total_proxy_num = len(proxy_list)
-print("got %d proxies to check" % total_proxy_num)
-
-if total_proxy_num == 0:
-    exit()
-
-# Wait for queue to get empty
-input_queue.join()
-result_queue.join()
-
-
-#while (not input_queue.empty()):
-#    time.sleep(1)
-
-
-# Shutdown
-f_printer.terminate()
-
-for worker in workers:
-    worker.terminate()
-
-# Print some info
-good_proxy_num = float(next(good_proxy_num))
-print("In: %d. Good: %d, that's %.2f%%" % (total_proxy_num, good_proxy_num, 100.0 * good_proxy_num/total_proxy_num))
-
-end_time = time.time()
-print("Time elapsed: %.1f seconds." % (end_time - start_time))
-print("Bye-bye!")
+for i in range(15):
+    threading.Thread(target=send_post).start()
 
